de34fe1b5c255e562e881a0cfbce59880368e8cd
keindev <kein@tagproject.ru>
2021-03-02 01:19:33 +0300

    build: bump eslint to 7.21.0

--- src/Builder.ts
+++ src/Builder.ts
@@ -1,131 +1,131 @@
-import path from 'path';
+import figures from 'figures';
 import fs from 'fs';
 import globby from 'globby';
-import readPkg, { PackageJson } from 'read-pkg';
 import gh from 'parse-github-url';
-import figures from 'figures';
+import path from 'path';
+import readPkg, { PackageJson } from 'read-pkg';
 import { UpdateManager } from 'stdout-update';
 
 const manager = UpdateManager.getInstance();
 const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
 
 export enum AvailableFiles {
-    Icon = 'icon',
-    Logo = 'logo',
-    Demo = 'demo',
-    Preview = 'social-preview',
+  Icon = 'icon',
+  Logo = 'logo',
+  Demo = 'demo',
+  Preview = 'social-preview',
 }
 
 export interface ILinks {
-    git?: string;
-    npm?: string;
-    homepage?: string;
+  git?: string;
+  npm?: string;
+  homepage?: string;
 }
 
 export interface IInfoFile {
-    name: string;
-    repo: string;
-    version: string;
-    type?: string;
-    description: string;
-    keywords?: string[];
-    links: ILinks;
-    files: {
-        [key in AvailableFiles]?: string;
-    };
+  name: string;
+  repo: string;
+  version: string;
+  type?: string;
+  description: string;
+  keywords?: string[];
+  links: ILinks;
+  files: {
+    [key in AvailableFiles]?: string;
+  };
 }
 
 export class Builder {
-    private dir: string;
-    private type: string;
-    private message = '';
-    private timer: NodeJS.Timeout | null = null;
-    private frameIndex = 0;
+  private dir: string;
+  private type: string;
+  private message = '';
+  private timer: NodeJS.Timeout | null = null;
+  private frameIndex = 0;
 
-    constructor(dir: string, type: string) {
-        this.dir = path.relative(process.cwd(), dir);
-        this.type = type;
-    }
+  constructor(dir: string, type: string) {
+    this.dir = path.relative(process.cwd(), dir);
+    this.type = type;
+  }
 
-    public async generate(): Promise<void> {
-        this.start();
+  public async generate(): Promise<void> {
+    this.start();
 
-        try {
-            const paths = await globby([`${this.dir}/**/*.*`], { gitignore: false });
-            const pkg = await readPkg({ normalize: false });
-            const url = typeof pkg.repository === 'object' ? pkg.repository.url : pkg.repository;
+    try {
+      const paths = await globby([`${this.dir}/**/*.*`], { gitignore: false });
+      const pkg = await readPkg({ normalize: false });
+      const url = typeof pkg.repository === 'object' ? pkg.repository.url : pkg.repository;
 
-            if (url) {
-                const git = gh(url);
+      if (url) {
+        const git = gh(url);
 
-                if (git && git.repo) {
-                    if (paths.length) {
-                        await fs.promises.writeFile(
-                            path.relative(process.cwd(), '.ghinfo'),
-                            JSON.stringify(this.build(paths, pkg, git.repo), null, 4)
-                        );
-                    }
+        if (git && git.repo) {
+          if (paths.length) {
+            await fs.promises.writeFile(
+              path.relative(process.cwd(), '.ghinfo'),
+              JSON.stringify(this.build(paths, pkg, git.repo), null, 4)
+            );
+          }
 
-                    this.end();
-                } else {
-                    throw new Error('Invalid package repository url!');
-                }
-            } else {
-                throw new Error('Package repository is undefined!');
-            }
-        } catch (error) {
-            this.error(error);
+          this.end();
+        } else {
+          throw new Error('Invalid package repository url!');
         }
+      } else {
+        throw new Error('Package repository is undefined!');
+      }
+    } catch (error) {
+      this.error(error);
     }
+  }
 
-    public build(paths: string[], pkg: PackageJson, repo: string): IInfoFile {
-        const { name, version, description, homepage, keywords } = pkg;
-        const availableFiles = Object.values(AvailableFiles);
+  public build(paths: string[], pkg: PackageJson, repo: string): IInfoFile {
+    const { name, version, description, homepage, keywords } = pkg;
+    const availableFiles = Object.values(AvailableFiles);
 
-        if (!name) throw new Error('Package name is undefined!');
-        if (!version) throw new Error('Package name is undefined!');
-        if (!description) throw new Error('Package name is undefined!');
+    if (!name) throw new Error('Package name is undefined!');
+    if (!version) throw new Error('Package name is undefined!');
+    if (!description) throw new Error('Package name is undefined!');
 
-        return {
-            name,
-            version,
-            description,
-            keywords,
-            repo,
-            type: this.type,
-            links: {
-                git: `https://github.com/${repo}`,
-                ...(pkg.isPrivate ? {} : { npm: `https://www.npmjs.com/package/${name}` }),
-                ...(homepage ? { homepage } : {}),
-            },
-            files: paths.reduce((acc, filePath) => {
-                const { name: fileName } = path.parse(filePath);
+    return {
+      name,
+      version,
+      description,
+      keywords,
+      repo,
+      type: this.type,
+      links: {
+        git: `https://github.com/${repo}`,
+        ...(pkg.isPrivate ? {} : { npm: `https://www.npmjs.com/package/${name}` }),
+        ...(homepage ? { homepage } : {}),
+      },
+      files: paths.reduce((acc, filePath) => {
+        const { name: fileName } = path.parse(filePath);
 
-                return ~availableFiles.indexOf(fileName as AvailableFiles) ? { ...acc, [fileName]: filePath } : acc;
-            }, {}),
-        };
+        return ~availableFiles.indexOf(fileName as AvailableFiles) ? { ...acc, [fileName]: filePath } : acc;
+      }, {}),
+    };
+  }
+
+  private start(): void {
+    manager.hook();
+    this.timer = setInterval(() => {
+      const frame = frames[(this.frameIndex = ++this.frameIndex % frames.length)];
+
+      manager.update([`${frame} process: ${this.message}`]);
+    }, 80);
+  }
+
+  private end(msg = [`${figures.tick} .ghinfo created!`]): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+      manager.update(msg, 0);
+      manager.unhook(false);
     }
+  }
 
-    private start(): void {
-        manager.hook();
-        this.timer = setInterval(() => {
-            const frame = frames[(this.frameIndex = ++this.frameIndex % frames.length)];
+  private error(error: Error): never {
+    this.end([`${figures.cross} Error!`]);
 
-            manager.update([`${frame} process: ${this.message}`]);
-        }, 80);
-    }
-
-    private end(msg = [`${figures.tick} .ghinfo created!`]): void {
-        if (this.timer) {
-            clearInterval(this.timer);
-            manager.update(msg, 0);
-            manager.unhook(false);
-        }
-    }
-
-    private error(error: Error): never {
-        this.end([`${figures.cross} Error!`]);
-
-        throw error;
-    }
+    throw error;
+  }
 }
--- src/__tests__/Builder.test.ts
+++ src/__tests__/Builder.test.ts
@@ -6,28 +6,28 @@ const paths = ['media/logo.svg', 'media/logo.jpg', 'media/social-preview.png'];
 let builder: Builder;
 
 describe('Builder', () => {
-    describe('Generate .ghinfo file', () => {
-        beforeEach(() => {
-            builder = new Builder(process.cwd(), type);
-        });
-
-        it('Build media info structure', () => {
-            const pkg = { name: 'media-info', version: '1.0.0', description: 'test', keywords: ['test'] };
-            const info = builder.build(paths, pkg, repo);
-
-            expect(info).toStrictEqual({
-                ...pkg,
-                repo,
-                type,
-                links: {
-                    git: `https://github.com/${repo}`,
-                    npm: `https://www.npmjs.com/package/${pkg.name}`,
-                },
-                files: {
-                    logo: 'media/logo.jpg',
-                    'social-preview': 'media/social-preview.png',
-                },
-            });
-        });
+  describe('Generate .ghinfo file', () => {
+    beforeEach(() => {
+      builder = new Builder(process.cwd(), type);
     });
+
+    it('Build media info structure', () => {
+      const pkg = { name: 'media-info', version: '1.0.0', description: 'test', keywords: ['test'] };
+      const info = builder.build(paths, pkg, repo);
+
+      expect(info).toStrictEqual({
+        ...pkg,
+        repo,
+        type,
+        links: {
+          git: `https://github.com/${repo}`,
+          npm: `https://www.npmjs.com/package/${pkg.name}`,
+        },
+        files: {
+          logo: 'media/logo.jpg',
+          'social-preview': 'media/social-preview.png',
+        },
+      });
+    });
+  });
 });
